cc482db54f74b12bffe8f7513793946ada6401c3
Muhammad Zain <Zainsaleem456@gmail.com>
2022-01-31 18:48:41 +0500

    Updates

---
+++ precache-manifest.10add0dd9206eba504982fb4f0e71179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e067435436a672a0f4691a460596c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fed73c3ab41b00855ae8",
+    "url": "./static/css/main.1c6f4970.chunk.css"
+  },
+  {
+    "revision": "bf6e3e35730f1b9f2d55",
+    "url": "./static/js/2.c9fb5a06.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c9fb5a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fed73c3ab41b00855ae8",
+    "url": "./static/js/main.5365ecd7.chunk.js"
+  },
+  {
+    "revision": "a12e4e6d3afb7ba5c57e",
+    "url": "./static/js/runtime-main.e32b8cf6.js"
+  }
+]);
